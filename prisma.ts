61d44afcddf6560d824dc0b2b789d09f1adaf74b
Joost Nibbeling <joost@vidicode.com>
2025-11-26 11:54:39 +0000

    Prisma 6

--- prisma.ts
+++ prisma.ts
@@ -1,11 +1,8 @@
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from './generated/prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`
 
-const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({
-  adapter, log: [
+  log: [
     {
       emit: 'event',
       level: 'query',
